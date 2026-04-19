4358845f60126626a2c66e4cb67574640dfefe27
rajeevJha-0700 <rajeevranjanjha0001@gmail.com>
2026-04-20 01:12:03 +0530

    add: backendURI

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: '',                 //we are using credentials   
+    baseURL: 'https://streamline-backend-3kuo.onrender.com',        
     withCredentials: true        
 });
 
