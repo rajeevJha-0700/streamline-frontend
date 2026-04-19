2dd1b9bbbc243b0c132af809f5f37e7f77c4c15e
rajeevJha-0700 <rajeevranjanjha0001@gmail.com>
2026-04-20 02:24:52 +0530

    removed proxy

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',   // Your backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:4000',   // Your backend port
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
   },
 })
